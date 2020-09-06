0de702e95252146b9146b60241bb811948364624
迷渡 <justjavac@gmail.com>
2020-09-06 12:12:59 +0800

    upgrade to deno@1.3.3 (#4)
    * upgrade to deno@1.3.3
    
    * upgrade deno@1.3
    
    * upgrade std@0.68.0
    
    * upgrade pagic@0.8.7
    
    * 修改 pagic 配置

--- codes/dev_deps.ts
+++ codes/dev_deps.ts
@@ -1,2 +1,2 @@
 // 使用 dev_deps.ts 文件来管理所有的第三方脚本
-export * from "https://deno.land/std@v0.57.0/testing/asserts.ts";
+export * from "https://deno.land/std@v0.68.0/testing/asserts.ts";
--- pagic.config.ts
+++ pagic.config.ts
@@ -1,33 +1,35 @@
 export default {
   srcDir: ".",
+  outDir: "public",
+  include: ["README.md", "docs"],
   ignore: [
-    /\/codes\//,
-    /\/public\//,
-    /\/\.git\//,
-    /\/\.github\//,
-    /\/\.vscode\//,
-    /\/LICENSE/,
-    /\/pagic\.config\.ts/,
+    "codes",
+    "LICENSE",
   ],
   theme: "docs",
   plugins: ["sidebar", "script", "ga"],
   title: "Deno 入门指南",
-  sidebar: [
-    "README.md",
-    "docs/install-deno.md",
-    "docs/setup-environment.md",
-  ],
+  github: "justjavac/deno_handbook",
+  sidebar: {
+    "/": ["README.md", "docs/install-deno.md", "docs/setup-environment.md"],
+  },
   nav: [
     {
-      text: "GitHub",
-      link: "https://github.com/justjavac/deno_handbook",
+      text: "社区",
+      link: "https://deno.js.cn",
+      target: '_blank',
     },
     {
-      text: "社区",
-      link: "http://deno.js.cn",
+      text: "安装",
+      link: "https://x.deno.js.cn",
+      target: '_blank',
     },
   ],
   ga: {
     id: "UA-123999279-3",
+  },
+  tools: {
+    editOnGithub: true,
+    backToTop: true,
   },
 };
